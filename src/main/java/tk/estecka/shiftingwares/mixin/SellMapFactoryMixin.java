7d92fcb5d0b835439fd4517e3e03aaa1746f7648
Estecka <estecka@galacsys.com>
2023-09-14 15:02:06 +0200

    Fix missing map name and colour

--- src/main/java/tk/estecka/shiftingwares/mixin/SellMapFactoryMixin.java
+++ src/main/java/tk/estecka/shiftingwares/mixin/SellMapFactoryMixin.java
@@ -8,6 +8,8 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.item.map.MapIcon;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.village.TradeOffer;
 import net.minecraft.village.TradeOffers.SellMapFactory;
@@ -22,6 +24,8 @@ implements ISellMapFactoryDuck
 	@Shadow @Final private int price;
 	@Shadow @Final private int maxUses;
 	@Shadow @Final private int experience;
+	@Shadow @Final private String nameKey;
+	@Shadow @Final private MapIcon.Type iconType;
 
 	@Shadow public abstract TradeOffer	create(Entity entity, Random random);
 
@@ -34,6 +38,9 @@ implements ISellMapFactoryDuck
 			ShiftingWares.LOGGER.info("Reselling a previously available map #{} in slot {}", cachedId.getAsInt(), tradeIndex);
 			ItemStack stack = new ItemStack(Items.FILLED_MAP);
 			IFilledMapItemMixin.callSetMapId(stack, cachedId.getAsInt());
+			stack.setCustomName(Text.translatable(this.nameKey));
+			if (iconType.hasTintColor())
+				stack.getOrCreateSubNbt("display").putInt("MapColor", iconType.getTintColor());
 			return new TradeOffer( new ItemStack(Items.EMERALD, this.price), new ItemStack(Items.COMPASS), stack, this.maxUses, this.experience, 0.2f );
 		}
 		else{
