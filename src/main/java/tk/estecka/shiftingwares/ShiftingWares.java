22fa74d93e4df47d6cc6396bf8e1f5bc1b4fc32e
Estecka <estecka@galacsys.com>
2024-01-29 11:25:57 +0100

    Trade rebalance availablility check.

--- src/main/java/tk/estecka/shiftingwares/ShiftingWares.java
+++ src/main/java/tk/estecka/shiftingwares/ShiftingWares.java
@@ -6,6 +6,8 @@ import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.resource.featuretoggle.FeatureFlags;
+import net.minecraft.util.Identifier;
 import net.minecraft.village.TradeOffer;
 import net.minecraft.village.TradeOffers;
 import net.minecraft.world.GameRules;
@@ -21,6 +23,9 @@ implements ModInitializer
 {
 	static public final Logger LOGGER = LoggerFactory.getLogger("Shifting-Wares");
 
+	static public final Identifier TRADE_REBALANCE_FLAGID = new Identifier("minecraft", "trade_rebalance");
+	static public final boolean IS_EXP_TRADE_AVAILABLE;
+
 	static public final GameRules.Key<BooleanRule> DAILY_RULE    = GameRuleRegistry.register("shiftingWares.dailyReroll",    GameRules.Category.MOBS, GameRuleFactory.createBooleanRule(true));
 	static public final GameRules.Key<BooleanRule> DEPLETED_RULE = GameRuleRegistry.register("shiftingWares.depleteReroll",  GameRules.Category.MOBS, GameRuleFactory.createBooleanRule(true));
 	static public final GameRules.Key<BooleanRule> MAP_RULE      = GameRuleRegistry.register("shiftingWares.allowMapReroll", GameRules.Category.MOBS, GameRuleFactory.createBooleanRule(false));
@@ -28,6 +33,12 @@ implements ModInitializer
 	static public final TradeOffer PLACEHOLDER_TRADE = new TradeOffer(ItemStack.EMPTY, ItemStack.EMPTY, ItemStack.EMPTY, 0, 0, 0, 0, 0);
 
 	static public final ITradeLayoutProvider VANILLA_LAYOUT  = new VanillaTradeLayout();
+
+	static {
+		var featureSet = FeatureFlags.FEATURE_MANAGER.getFeatureSet();
+		var featureIds = FeatureFlags.FEATURE_MANAGER.toId(featureSet);
+		IS_EXP_TRADE_AVAILABLE = featureIds.contains(TRADE_REBALANCE_FLAGID);
+	}
 
 	static public List<TradeOffers.Factory[]> GetTradeLayout(VillagerEntity villager){
 		var providers = FabricLoader.getInstance().getEntrypoints("shifting-wares", ITradeLayoutProvider.class);
@@ -44,5 +55,6 @@ implements ModInitializer
 	@Override
 	public void onInitialize() {
 		// Static initialization
+		LOGGER.warn("Trade rebalance is : {}", IS_EXP_TRADE_AVAILABLE?"available":"unavailable");
 	}
 }
