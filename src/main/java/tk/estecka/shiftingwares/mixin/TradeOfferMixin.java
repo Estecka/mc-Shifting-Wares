db92803c09984f5e43e387b534c3261ce27a29a0
Estecka <estecka@galacsys.com>
2024-11-04 21:18:22 +0100

    TradeOffer codec extension

--- src/main/java/tk/estecka/shiftingwares/mixin/TradeOfferMixin.java
+++ src/main/java/tk/estecka/shiftingwares/mixin/TradeOfferMixin.java
@@ -1,7 +1,13 @@
 package tk.estecka.shiftingwares.mixin;
 
+import java.util.function.Function;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.village.TradeOffer;
 import tk.estecka.shiftingwares.ShiftingTradeData;
 import tk.estecka.shiftingwares.duck.ITradeOfferDuck;
@@ -11,7 +17,7 @@ import tk.estecka.shiftingwares.duck.ITradeOfferDuck;
 public class TradeOfferMixin
 implements ITradeOfferDuck
 {
-	private ShiftingTradeData data = null;
+	private ShiftingTradeData data = new ShiftingTradeData();
 
 	@Override
 	public ShiftingTradeData shiftingwares$GetTradeData(){
@@ -21,5 +27,20 @@ implements ITradeOfferDuck
 	@Override
 	public void shiftingwares$SetTradeData(ShiftingTradeData data){
 		this.data = data;
+	}
+
+	@ModifyExpressionValue( method="<clinit>", at=@At(value="INVOKE", remap=false, target="com/mojang/serialization/codecs/RecordCodecBuilder.create(Ljava/util/function/Function;)Lcom/mojang/serialization/Codec;") )
+	static private Codec<TradeOffer> ExtendCodec(Codec<TradeOffer> original)
+	{
+		return RecordCodecBuilder.create(instance -> 
+			instance.group(
+				MapCodec.assumeMapUnsafe(original).forGetter(Function.identity()),
+				ShiftingTradeData.CODEC.fieldOf("shiftingwares:tradeData").orElseGet(()->new ShiftingTradeData()).forGetter(offer -> ITradeOfferDuck.Of(offer).shiftingwares$GetTradeData())
+			)
+			.apply(instance, (offer,data)->{
+				ITradeOfferDuck.Of(offer).shiftingwares$SetTradeData(data);
+				return offer;
+			})
+		);
 	}
 }
